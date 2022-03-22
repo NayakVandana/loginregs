176e96ffe2ecfa98d8677fe3e53210efee10dcf8
Vandana Nayak <95909398+NayakVandana@users.noreply.github.com>
2022-03-22 14:30:33 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 app.use(cors());
 const AuthController = require("./controller/authController");
+app.get("/",(req,res)=>{
+    res.send("<h1>Hello From Home Page</h1>");
+})
+
+
 
 app.use("/api/auth",AuthController);
 
--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://test:test123@cluster0.cxejl.mongodb.net/zomato?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://test:test123@cluster0.rjvna.mongodb.net/zomato?retryWrites=true&w=majority");
